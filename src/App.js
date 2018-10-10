a6f9a796f398a1a71f745ae7c1340bcb0d5ba5f7
leejm1 <wnsaud5415@gamil.com>
2018-10-10 21:03:23 +0900

    init

--- src/App.js
+++ src/App.js
@@ -3,23 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
+    const style = {
+      backgroundColor: 'black',
+      padding: '16px',
+      color: 'white',
+      fontSize: '12px'
+    };
+    const aaa="aaa";
     return (
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        hi there {aaa}
       </div>
     );
   }
---
+++ src/My.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MyName from './MyName';
+
+class My extends Component {
+  render() {
+    return (
+      <MyName name="리액트" />
+    );
+  }
+}
+
+export default App;
